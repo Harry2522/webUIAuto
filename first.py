0fc6aebb674311bcb6e94b669b10292fbf373ed9
hrzhang <zhr10187@163.com>
2020-07-15 14:11:36 +0800

    添加文件first.py

---
+++ first.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName:first.py
+# @Time    :2020/7/15 14:09
+# @Author  :Harry
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://www.baidu.com")
